b9adee00ce94abf1d71add9c3e68b7ff4104f034
William <william@undefined.re>
2022-01-03 20:17:16 +0100

    enterprise without wg

--- config/system/enterprise-config.scm
+++ config/system/enterprise-config.scm
@@ -4,34 +4,73 @@
              (gnu packages networking)
              (gnu packages xdisorg)
              (gnu system setuid)
+             (nongnu packages linux)
+             (nongnu system linux-initrd)
              (guix gexp)
              (ice-9 format)
              (srfi srfi-1)
-             (srfi srfi-88))
+             (srfi srfi-88)
+             ;; (w7)
+             )
+
+;; The following code is from
+;; [[https://www.draketo.de/software/guile-capture-stdout-stderr.html]].
+
+;; Comments and some formatting by me.
+
+;; Related links:
+
+;; https://www.gnu.org/software/guile/manual/html_node/Pipes.html
+;; https://www.gnu.org/software/guile/manual/guile.html#Ports-and-File-Descriptors
+
+;; This is an example of how you can write a procedure, which allows you to run
+;; a shell command from GNU Guile and capture not only its stdout output in a
+;; string, but also its stderr output in a string. This can be useful, if you
+;; need to parse the output of both stdout and stderr.
 
 ;; Import nonfree linux module.
 (use-modules (nongnu packages linux)
              (nongnu system linux-initrd))
 
-(define host
-  (let ((host-str (getenv "HOST")))
-    (cond ((or (string=? host-str "enterprise")
-               (string=? host-str "yggdrasill"))
-           (begin
-             (display (string-append "building for " host-str "\n"))
-             (string->keyword host-str)))
-          (#t (error (string-append "unknown host: " host-str "\n") 69)))))
-
 (use-service-modules desktop networking ssh xorg docker)
 
-(define machine-config '((#:enterprise .
-                          ((#:uuids .
-                            ((#:vault . "125bf330-ff27-45d1-9cce-1dd96cb14975")
-                             (#:efi . "6C21-E416")))))
-                         (#:yggdrasill .
-                          ((#:uuids .
-                            ((#:vault . "077c1391-b290-4921-ae90-f8e3cec68113")
-                             (#:efi . "77DE-0AE2")))))))
+(define hostname (getenv "HOST"))
+(define host
+  (cond ((or (string=? hostname "enterprise")
+             (string=? hostname "yggdrasill"))
+         (begin
+           (display (string-append "building for " hostname "\n"))
+           (string->keyword hostname)))
+        (#t (error (string-append "unknown host: " hostname "\n") 69))))
+
+;; (define pass-get (lambda* (path #:optional hostname)
+;;                    (let ((path (if hostname
+;;                                    (string-append "fleet/" hostname "/" path)
+;;                                    path)))
+;;                      (display path)
+;;                      (call-command-with-output-error-to-string (string-append "pass show " path)))))
+
+;; (let ((t1 '((#:lol . ((#:kkt . "ohoh"))))))
+;;   (display (nassq '(#:lol #:kkt) t1)))
+;; (fold (lambda (k wg-conf)
+;;         (alist-cons k (pass-get (keyword->string k) hostname) wg-conf))
+;;       '()
+;;       '(#:public #:private #:psk))
+;; (iota 5)
+
+;; (define secrets '((#:wireguard . (#:public . ))))
+
+(define machine-config
+  (let ((initial  '((#:enterprise .
+                     ((#:uuids .
+                       ((#:vault . "125bf330-ff27-45d1-9cce-1dd96cb14975")
+                        (#:efi . "6C21-E416")))))
+                    (#:yggdrasill .
+                     ((#:uuids .
+                       ((#:vault . "077c1391-b290-4921-ae90-f8e3cec68113")
+                        (#:efi . "77DE-0AE2"))))))))
+    initial))
+
 (define (nassq alist ks)
   (fold (lambda (k al) (assq-ref al k)) alist ks))
 
@@ -76,8 +115,22 @@
                     (host "0.0.0.0")
                     (port 1691)
                     (advertise? #t)))
+          ;; (simple-service 'wireguard-module
+          ;;                 kernel-module-loader-service-type
+          ;;                 '("wireguard"))
+          ;; (service wireguard-service-type
+          ;;          (wireguard-configuration
+          ;;           (peers
+          ;;            (list
+          ;;             (wireguard-peer
+          ;;              (name "my-peer")
+          ;;              (endpoint "my.wireguard.com:51820")
+          ;;              (public-key "hzpKg9X1yqu1axN6iJp0mWf6BZGo8m1wteKwtTmDGF4=")
+          ;;              (allowed-ips '("10.0.0.2/32")))))))
           (modify-services %desktop-services
                            (delete gdm-service-type))))
+
+  ;;(kernel-loadable-modules (list wireguard-linux-compat));; FIXME
 
   (setuid-programs
    (cons*
@@ -135,7 +188,7 @@
            (type "vfat"))
           %base-file-systems))
   (swap-devices
-   '("/swap/swapfile")) ;; FIXME
+   '("/mnt/vault/swap/swapfile")) ;; FIXME
   (bootloader
    (bootloader-configuration
     (bootloader grub-efi-bootloader)
